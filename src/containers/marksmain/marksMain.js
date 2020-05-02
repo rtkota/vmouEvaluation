d9082b3390d4a42cd4aa799fc1544bbd3d2e665f
rtkota <tallykota@gmail.com>
2020-05-02 10:01:10 +0530

    MainPage

--- src/containers/marksmain/marksMain.js
+++ src/containers/marksmain/marksMain.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import classes from './marksmain.module.css';
+import vmouLogo from '../../assets/logo.png'
 
 class MarksMain extends Component {
     render () {
         return (
-            <div>
-
+            <div className={classes.App}>
+                <div className={classes.card}>
+                    <img className={classes.logo} fluid src={vmouLogo} alt=" "/>
+                </div>
+                
+                <h1>Important Instructions</h1>
             </div>
         )
     }
